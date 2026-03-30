9d2512bbb715b9787352ee089339b3ee6a37d5af
Rocco Davino <jasonroc19@gmail.com>
2026-03-30 07:04:29 -0500

    refactor(frontend): rename TransactionsPage to follow PascalCase convention

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Dashboard from './Dashboard'
-import TransactionsPage from './transactions-page'
+import TransactionsPage from './TransactionsPage'
 import ConfigPage from './ConfigPage'
 
 createRoot(document.getElementById('root')!).render(
